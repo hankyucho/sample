3060f893b4788abf24b9fa9925f1738ec7ddee87
goodee <goodee@DESKTOP-PDAJSH9>
2018-11-01 16:11:10 +0900

    asd

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -2,8 +2,8 @@
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello~~");
-		System.out.println("Hello!!");
+		System.out.println("Hello~~~");
+		System.out.println("Hello!!!");
 	}
 
 }
