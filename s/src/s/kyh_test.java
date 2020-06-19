f3e2edca496a49ec8816efedbb49d88d517c37cc
kimyeonghoon <me@yeonghoon.kim>
2020-06-19 16:18:22 +0900

    ㅁㅁㅁㅁ

--- s/src/s/kyh_test.java
+++ s/src/s/kyh_test.java
@@ -4,6 +4,10 @@ public class kyh_test {
 
 	public static void main(String[] args) {
 		System.out.println("test0000");
+		System.out.println("test0000");
+		System.out.println("test0000");
+		System.out.println("test0000");
+		System.out.println("test0000");
 	}
 
 }
