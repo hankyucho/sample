42d14d25708072265ae55860b669c59dd7a81585
GDJ23 <GDJ23@DESKTOP-RKCH88A>
2020-06-19 15:57:47 +0900

    test

--- s/src/s/wussupchung.java
+++ s/src/s/wussupchung.java
@@ -2,11 +2,8 @@ package s;
 
 public class wussupchung {
 
-		public static void main(String[] args) {
-			System.out.println("WussupChung");
-			System.out.println("하이요");
-			System.out.println("영업조입니다.");
-			System.out.println("조원은 지금 3명뿐이 없어요 ");
-		}
-
+	public static void main(String[] args) {
+		System.out.println("WussupChung");
 	}
+
+}
