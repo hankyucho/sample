1ceb3ecbf17429e47269a5e1bd91876cd64789ad
GDJ23 <GDJ23@DESKTOP-TJ6I2M5>
2020-06-19 14:45:45 +0900

    커밋 완료

--- s/src/s/Test.java
+++ s/src/s/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("맛스타 변신");
 		System.out.println("맛스타라니.. ㅠㅠ 이젠 맛스타 안나와유!!");
+		System.out.println("마스터!!");
 	}
 
 }
