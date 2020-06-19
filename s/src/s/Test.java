7617920dff80284c8cdf4b98588d533c997ee1d5
GDJ23 <GDJ23@DESKTOP-RKCH88A>
2020-06-19 15:17:40 +0900

    영영업업

--- s/src/s/Test.java
+++ s/src/s/Test.java
@@ -3,8 +3,10 @@ package s;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("WussupChung");
-		System.out.println("맛스타 변신");
+		System.out.println("맛스타 변신2!");
+		System.out.println("하이요");
+		System.out.println("영업조입니다.");
+		System.out.println("조원은 지금 3명뿐이 없어요 ");
 	}
 
 }
