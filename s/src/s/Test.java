b3bdea77028adca9693bd733ef8565a13f62cd41
GDJ <GDJ@DESKTOP-C5R1TBR>
2020-06-19 15:54:28 +0900

    1+2+3+..+9+10

--- s/src/s/Test.java
+++ s/src/s/Test.java
@@ -5,6 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Mir");
 		System.out.println("맛스타 변신");
+		
+		int sum = 0;
+		for(int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
 	}
-
 }
